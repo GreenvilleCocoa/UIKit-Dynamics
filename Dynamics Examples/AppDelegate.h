38da14ac6595eb87c081db56bef0c61fc545e636
Ryan Poolos <ryanpoolos@Gmail.com>
2014-07-07 23:03:53 -0400

    Initial Commit

---
+++ Dynamics Examples/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Dynamics Examples
+//
+//  Created by Ryan Poolos on 7/7/14.
+//  Copyright (c) 2014 Frozen Fire Studios, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Dynamics Examples/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Dynamics Examples
+//
+//  Created by Ryan Poolos on 7/7/14.
+//  Copyright (c) 2014 Frozen Fire Studios, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
